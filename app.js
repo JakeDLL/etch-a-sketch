b17dd5fa2eccdabc603186a7f81233c02005cba4
JakeDLL <35861176+JakeDLL@users.noreply.github.com>
2024-10-22 18:20:13 -0400

    Add: clearGrid function to clear grid

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const clearBtn = document.querySelector('#clear-btn');
+const changeBtn = document.querySelector('#change-btn');
 const gridContainer = document.querySelector('.grid');
+
+function clearGrid() {
+	const boxes = document.querySelectorAll('.box');
+	boxes.forEach(box => box.style.backgroundColor = 'white');
+}
+
+clearBtn.addEventListener('click', clearGrid);
+
 for (let col = 0; col < 16; col++) {
 	let column = document.createElement('div');
 	column.classList = 'column';
@@ -6,9 +16,16 @@ for (let col = 0; col < 16; col++) {
 	for (let boxNum = 0; boxNum < 16; boxNum++) {
 		let box = document.createElement('div');
 		box.classList = 'box';
-		box.textContent = `Column: ${col + 1} || Box: ${boxNum + 1}`;
 		box.id = `box${boxNum + 1}`;
+		
+		box.addEventListener('mouseover', event => {
+			const target = event.target;
+			target.style.backgroundColor = 'black';
+		});
+
 		column.appendChild(box);
 	}
 	gridContainer.appendChild(column);
 }
+
+
