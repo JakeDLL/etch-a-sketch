b7252f21fcc0438d18acccd1d4837295c8ad6db8
Raul Del Llano <35861176+JakeDLL@users.noreply.github.com>
2024-10-22 21:59:27 -0400

    Add: button function to change grid size
    User will input a number. Event handler will check
    for NaN, 0, or > 100 numbers then pass the number
    to changeGrid function.
    
    Change grid function will create n columns and n boxes
    per column then append the columns to grid container.

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ const changeBtn = document.querySelector('#change-btn');
 const gridContainer = document.querySelector('.grid');
 
 function changeGrid(size) {
+	gridContainer.innerHTML = '';
 
+	for (let col = 0; col < size; col++) {
+		let column = document.createElement('div');
+		column.classList = 'column';
+		column.id = `column${col + 1}`;
+		for (let boxNum = 0; boxNum < size; boxNum++) {
+			let box = document.createElement('div');
+			box.classList = 'box';
+			box.id = `box${boxNum + 1}`;
+
+			box.addEventListener('mouseover', event => {
+				const target = event.target;
+				target.style.backgroundColor = 'black';
+			});
+			
+			column.appendChild(box);
+		}
+		gridContainer.appendChild(column);
+	}
 }
 
 function clearGrid() {
@@ -12,24 +31,12 @@ function clearGrid() {
 }
 
 clearBtn.addEventListener('click', clearGrid);
-
-for (let col = 0; col < 16; col++) {
-	let column = document.createElement('div');
-	column.classList = 'column';
-	column.id = `column${col + 1}`;
-	for (let boxNum = 0; boxNum < 16; boxNum++) {
-		let box = document.createElement('div');
-		box.classList = 'box';
-		box.id = `box${boxNum + 1}`;
-		
-		box.addEventListener('mouseover', event => {
-			const target = event.target;
-			target.style.backgroundColor = 'black';
-		});
-
-		column.appendChild(box);
+changeBtn.addEventListener('click', () => {
+	let size = +prompt('What size grid?');
+	while (typeof size !== 'number' || size > 100 || size % 1 !== 0 || size <= 0) {
+		size = +prompt('Needs to be a whole number between 1 and 100');
 	}
-	gridContainer.appendChild(column);
-}
+	changeGrid(size);
+});
 
-
+changeGrid(16);
